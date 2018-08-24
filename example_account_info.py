1ebf9e7e0121248849d92b2d25cb6f9bcef490b4
Justin Hsi <justinm.hsi@gmail.com>
2018-08-24 15:18:16 -0700

    updated example_account_info.py to reflect changes to invest_script{_instant}.py. Notable changes are sending e-mails and writing to google spreadsheets loan counts in release batches.
    Former-commit-id: fdb5fd2a895248eb0052f06508b874028ab841df

--- example_account_info.py
+++ example_account_info.py
@@ -1,5 +1,10 @@
 email_throwaway = 'jane@gmail.com'
+from_email_throwaway = 'sending_email@gmail.com'
+to_emails_throwaway = ['list_of_receiving_emails@gmail.com']
 password_throwaway = 'test'
+project_path = '/path/to/../lendingclub'
+invest_ss_key = 'spreadsheet_key'
+investins_ss_key = 'instant_spreadsheet_key'
 
 investor_id = int('account_id_number')
 portfolio_id = int('portfolio_id_number')
