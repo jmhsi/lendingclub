6ec4bc973ec1394d8da468c0983d6b2d6a86f234
Justin Hsi <justinm.hsi@gmail.com>
2018-10-05 11:35:52 -0700

    made scripts for unzipping and merging loan info

---
+++ csv_dl_preparation/unzip_csvs.py
@@ -0,0 +1,26 @@
+import os
+import logging
+import pathlib
+import shutil
+import subprocess
+import lendingclub.csv_dl_preparation.download_prep as dp
+
+dpath = '/home/justin/all_data/lendingclub/csvs'
+working_csvs = os.path.join(dpath, 'working_csvs')
+if os.path.exists(working_csvs):
+    shutil.rmtree(working_csvs)
+os.makedirs(working_csvs)
+csv_path = dp.get_sorted_creationtime_dirs(dpath)[-1][1]
+zip_files = pathlib.Path(csv_path).rglob("*.zip")
+
+while True:
+    try:
+        path = next(zip_files)
+    except StopIteration:
+        print('all zip files have been unzipped')
+        break  # no more files
+    except PermissionError:
+        logging.exception("permission error")
+    else:
+        extract_dir = pathlib.Path(working_csvs)
+        subprocess.call(['unzip', '-o', path, '-d', extract_dir])
