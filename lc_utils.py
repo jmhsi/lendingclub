b6a7cadd8412e675156123c43c69e763760ba482
Justin Hsi <justinm.hsi@gmail.com>
2018-11-13 03:57:38 -0800

    standardized how to munge data via j_utils
    Former-commit-id: 39f832a6271b2238e82dbfb054f2e357e62f6dbc

---
+++ lc_utils.py
@@ -0,0 +1,40 @@
+# make a crude test set for now
+def get_split_date(df, date_column, quantile): 
+
+    """
+    https://stackoverflow.com/questions/31018622/pandas-quantile-function-for-dates
+    Get the date on which to split a dataframe for timeseries splitting
+    Adjusted coerce param to errors since SO is old.
+    """ 
+
+    # 1. convert date_column to datetime (useful in case it is a string) 
+    # 2. convert into int (for sorting) 
+    # 3. get the quantile 
+    # 4. get the corresponding date
+    # 5. return, pray that it works 
+
+    quantile_date = pd.to_datetime(df[date_column], errors = 'raise').astype('int64').quantile(q=quantile)#.astype('datetime64[ns]')
+
+    return pd.to_datetime(quantile_date)
+
+def split_out_traintestable_loans(base_loan_info, eval_loan_info, oldness_thrsh=.9):
+    '''Can train/test on loans that pass the oldness_thrsh or have status paid/defaulted/charged_off'''
+    old_enough_ids = eval_loan_info[(eval_loan_info['maturity_time_stat_adj'] >= oldness_thrsh) | 
+                                    (eval_loan_info['maturity_paid_stat_adj'] >= oldness_thrsh) | 
+                                    (eval_loan_info['loan_status'].isin(['paid', 'defaulted', 'charged_off']))]['id'].unique()
+    base_loan_info = base_loan_info[base_loan_info['id'].isin(old_enough_ids)]
+    eval_loan_info = eval_loan_info[eval_loan_info['id'].isin(old_enough_ids)]
+    return base_loan_info, eval_loan_info
+
+
+def add_custom_lc_features(df):
+    # added features
+    df['monthly_inc'] = df['annual_inc'] / 12
+    df['dti_w_loan'] = (df['dti'] * df['monthly_inc'] +
+                                    df['installment']) / df['monthly_inc']
+    df['delinq_to_monthly_inc'] = df['delinq_amnt'] / \
+        df['monthly_inc']
+    df['tot_cur_bal_to_monthly_inc'] = df['tot_cur_bal'] / \
+        df['monthly_inc']
+    df['loan_to_inc'] = df['loan_amount'] / \
+        df['monthly_inc']
