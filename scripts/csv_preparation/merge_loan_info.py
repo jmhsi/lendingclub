e92f33ed9bd846005d4279a2a5021f79d6e07a7d
Justin Hsi <justinm.hsi@gmail.com>
2019-06-14 13:40:54 -0700

    added a reduce memory step into merge_loan_info

--- scripts/csv_preparation/merge_loan_info.py
+++ scripts/csv_preparation/merge_loan_info.py
@@ -34,6 +34,10 @@ loan_info['term'] = loan_info['term'].str[:3].astype(int)
 loan_info = loan_info[(loan_info['int_rate'] > 0) & (loan_info['term'] > 0) &
                       (loan_info['funded_amnt'] > 0)]
 
+# compress memory
+sys.path.append(os.path.join(os.path.expanduser('~'), 'projects'))
+import j_utils.munging as mg
+changed_type_cols, loan_info = mg.reduce_memory(loan_info)
 
 # Reset index and set id to int______________________
 loan_info.reset_index(drop=True, inplace=True)
