b0abe76d2ae66ad708c2674bdb36fa5861d40596
Justin Hsi <justinm.hsi@gmail.com>
2019-10-02 21:43:34 -0700

    adjusted lenidngclub/modeling/models.py

--- lendingclub/config.py
+++ lendingclub/config.py
@@ -13,7 +13,8 @@ csv_dir = os.path.join(data_dir, 'csvs')
 raw_dl_dir = os.path.join(csv_dir, 'raw_zipped_csvs')
 arch_dir = os.path.join(csv_dir, 'archived_csvs')
 wrk_csv_dir = os.path.join(csv_dir, '02_working_csvs')
-
+modeling_dir = os.path.join(prj_dir, 'modeling')
+model_dir = os.path.join(modeling_dir, 'models')
 
 # print(srcdir)
 # print(prjdir)
