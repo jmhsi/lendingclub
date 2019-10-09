43b63e12a438c900d9fba4fe5d30f3b79d72c205
Justin Hsi <justinm.hsi@gmail.com>
2019-10-09 14:42:25 -0700

    added eval_loan_info.fth as dependency for 09_score

--- run/09_score.sh
+++ run/09_score.sh
@@ -2,6 +2,7 @@
 
 dvc run -d lendingclub/modeling/09_score.py \
     -d data/base_loan_info.fth \
+    -d data/eval_loan_info.fth \
     -d modeling/ \
     -o data/eval_loan_info_scored.fth \
     -f run/09_score.dvc \
