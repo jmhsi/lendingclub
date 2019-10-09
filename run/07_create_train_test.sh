49410f99226952393ec979ae5f7fea0f15c86289
Justin Hsi <justinm.hsi@gmail.com>
2019-10-09 14:32:36 -0700

    changed to pickle for bootstrap of dict of ids, seeded sampling for test set

--- run/07_create_train_test.sh
+++ run/07_create_train_test.sh
@@ -10,15 +10,6 @@ dvc run -d lendingclub/data_and_eval_preparation/07_create_train_test.py \
     -o data/train_eval_loan_info.fth \
     -o data/test_base_loan_info.fth \
     -o data/train_base_loan_info.fth \
-    -o data/test_eval_loan_info_0_bootstrap.fth \
-    -o data/test_eval_loan_info_1_bootstrap.fth \
-    -o data/test_eval_loan_info_2_bootstrap.fth \
-    -o data/test_eval_loan_info_3_bootstrap.fth \
-    -o data/test_eval_loan_info_4_bootstrap.fth \
-    -o data/test_eval_loan_info_5_bootstrap.fth \
-    -o data/test_eval_loan_info_6_bootstrap.fth \
-    -o data/test_eval_loan_info_7_bootstrap.fth \
-    -o data/test_eval_loan_info_8_bootstrap.fth \
-    -o data/test_eval_loan_info_9_bootstrap.fth \
+    -o data/bootstrap_test_eval_loan_info_ids.pkl \
     -f run/07_create_train_test.dvc \
     python lendingclub/data_and_eval_preparation/07_create_train_test.py 
