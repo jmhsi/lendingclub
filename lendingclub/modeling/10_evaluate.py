cfcaf99b389295480983a35e5d6e034d0e997326
Justin Hsi <justinm.hsi@gmail.com>
2019-10-11 21:07:05 -0700

    redid 07, removed scaled_pmt_hist as dependency

--- lendingclub/modeling/10_evaluate.py
+++ lendingclub/modeling/10_evaluate.py
@@ -22,14 +22,12 @@ parser.add_argument('--model', '-m', help='specify model(s) to train')
 if not len(sys.argv) > 1:
     model_n = 'baseline' # , 'A', 'B', 'C', 'D', 'E', 'F', 'G'
 
-def get_topn_ret(model, eval_df, n, return_col='0.07'): #, bootstrap=False
+def get_topn_ret(model, eval_df, n, return_col='0.07'): 
     '''
     Picks loans and get returns based on maximizing model_score
     '''
     assert n <= 1
     assert n >= 0
-#     if bootstrap:
-#         eval_df = eval_df.sample(frac=1, replace=True)
     return eval_df.nlargest(int(round(len(eval_df)*n)), f'{model}_score')['0.07'].mean()
 
 def get_topn_def_pct(model, eval_df, n): #, bootstrap=False
@@ -38,13 +36,12 @@ def get_topn_def_pct(model, eval_df, n): #, bootstrap=False
     '''
     assert n <= 1
     assert n >= 0
-#     if bootstrap:
-#         eval_df = eval_df.sample(frac=1, replace=True)
-    n_pick = int(round(len(eval_df)*n))
+    n_pick = max(1,int(round(len(eval_df)*n)))
     return eval_df.nlargest(n_pick, f'{model}_score')['target_strict'].sum()/n_pick
 
-def eval_model(model_n, scored, bs_ids):#, verbose=True, top_n=.05
+def eval_model(model_n, test, bs_idx):#, verbose=True, top_n=.05
     top_ns = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
+    issue_d_gr = test.groupby('issue_d')
     # make dicts to hold results
     total_top_n_ret_d = {}
     total_top_n_def_d = {}
@@ -53,20 +50,42 @@ def eval_model(model_n, scored, bs_ids):#, verbose=True, top_n=.05
     bsmbm_top_n_ret_d = {}
     bsmbm_top_n_def_d = {}
     
-    # overall top_n from whole test population
-    for top_n in top_ns:
-        top_n_ret = get_topn_ret(model_n, scored, top_n)
-        top_n_def = get_topn_def_pct(model_n, scored, top_n)
-        total_top_n_ret_d = {top_n: top_n_ret}
-        total_top_n_def_d = {top_n: top_n_def}
-    
-    
-    
+    for n in top_ns:
+         # overall top_n from whole test population
+        top_n_ret = get_topn_ret(model_n, test, n)
+        top_n_def = get_topn_def_pct(model_n, test, n)
+        
+        # month by month over all of test loans
+        temp_mbm = {}
+        temp_mbm_def = {}
+        for d, g in issue_d_gr:
+            temp_mbm[d] = get_topn_ret(model_n, g, n)
+            temp_mbm_def[d] = get_topn_def_pct(model_n, g, n)
+            
+        # get bsmbm
+        temp_bsmbm = {}
+        temp_bsmbm_def = {}
+        for i, idx in enumerate(bs_idx):
+            temp = {}
+            temp_def = {}
+            df = test.loc[idx]
+            for d, g in df.groupby('issue_d'):
+                temp[d] = get_topn_ret(model_n, g, n)
+                temp_def[d] = get_topn_def_pct(model_n, g, n)
+            temp_bsmbm[i] = temp
+            temp_bsmbm_def[i] = temp_def
+        
+        
+        bsmbm_top_n_ret_d[n] = temp_bsmbm
+        bsmbm_top_n_def_d[n] = temp_bsmbm_def
+        mbm_top_n_ret_d[n] = temp_mbm
+        mbm_top_n_def_d[n] = temp_mbm_def
+        total_top_n_ret_d[n] = top_n_ret
+        total_top_n_def_d[n] = top_n_def
+        
+        print(bsmbm_top_n_ret_d)
+        print(mbm_top_n_ret_d)
 
-#     if verbose:
-#         print('{0}'.format(model_n))
-#         print('topn return: {0}'.format(top_n_ret))
-#         print('topn default rate: {0}'.format(top_n_def))
         
     # SAVING ________________________________________________________________    
     # named stuff is non-tracked by dvc
@@ -88,16 +107,16 @@ if not os.path.isdir(config.results_dir):
     os.makedirs(config.results_dir)
 
 # load in datasets
-scored = pd.read_feather(os.path.join(config.data_dir, 'eval_loan_info_scored.fth'))
+test = pd.read_feather(os.path.join(config.data_dir, 'eval_loan_info_scored.fth'))
 # load in train_test_ids.pkl
 with open(os.path.join(config.data_dir, 'train_test_ids.pkl'), 'rb') as f:
     train_test_ids = pickle.load(f)
-scored = utils.cut_to_ids(scored, train_test_ids['test'])
+test = utils.cut_to_ids(test, train_test_ids['test'])
 # load in bootstrap_test_ids.pkl
 with open(os.path.join(config.data_dir, 'bootstrap_test_ids.pkl'), 'rb') as f:
     bootstrap_test_ids = pickle.load(f)
 
-eval_model(model_n, scored, bootstrap_test_ids)
+eval_model(model_n, test, bootstrap_test_ids)
     
     
     
