8c81a74777fdd3607d046411dba2b14cd707f4b1
Justin Hsi <justinm.hsi@gmail.com>
2019-10-14 23:55:43 -0700

    added argument parsing to 08/09/10

--- run/08_train.sh
+++ run/08_train.sh
@@ -4,4 +4,4 @@ dvc run -d lendingclub/modeling/08_train.py \
     -d data/train_test_ids.pkl \
     -o modeling/ \
     -f run/08_train.dvc \
-    python lendingclub/modeling/08_train.py 
+    python lendingclub/modeling/08_train.py -m baseline 
