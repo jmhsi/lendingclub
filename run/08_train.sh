00f261e95a96576ec8ac4a85c5462b6ec5f639cd
Justin Hsi <justinm.hsi@gmail.com>
2019-10-17 21:50:42 -0700

    ran logistic regression for run/08. 09 is next

--- run/08_train.sh
+++ run/08_train.sh
@@ -2,6 +2,8 @@
 
 dvc run -d lendingclub/modeling/08_train.py \
     -d data/train_test_ids.pkl \
+    -d data/base_loan_info.fth \
+    -d data/eval_loan_info.fth \
     -o modeling/ \
     -f run/08_train.dvc \
-    python lendingclub/modeling/08_train.py -m baseline 
+    python lendingclub/modeling/08_train.py -m logistic_regr 
