c8e802035741aa956df4c30d3a090bb516f203a2
Justin Hsi <justinm.hsi@gmail.com>
2019-10-20 02:33:42 -0700

    full run logistic_regr

--- run/10_evaluate.sh
+++ run/10_evaluate.sh
@@ -4,6 +4,7 @@ dvc run -d lendingclub/modeling/10_evaluate.py \
     -d data/eval_loan_info_scored.fth \
     -m results/default_rate.json \
     -m results/return.json \
-    -m results/smbm_return.json \
+    -m results/summary_mbm_return.json \
+    -m results/count.json \
     -f run/10_evaluate.dvc \
     python lendingclub/modeling/10_evaluate.py -m logistic_regr 
