0e6ff2f9448870e873008eb9be67c8b536274c9f
Justin Hsi <justinm.hsi@gmail.com>
2019-11-12 23:48:25 -0800

    remove modeling directory as a dependency

--- run/09_score.sh
+++ run/09_score.sh
@@ -3,7 +3,7 @@
 dvc run -d lendingclub/modeling/09_score.py \
     -d data/base_loan_info.fth \
     -d data/eval_loan_info.fth \
-    -d modeling/ \
+#    -d modeling/ \
     -d lendingclub/modeling/models.py \
     -o data/eval_loan_info_scored.fth \
     -f run/09_score.dvc \
