fa9284725bfc3ddc1cd94cd7220645dcd77ff096
Justin Hsi <justinm.hsi@gmail.com>
2019-12-01 15:49:01 -0800

    altered some of the run/__.sh files. reran full pipeline with 100k dataset

--- run/09_score.sh
+++ run/09_score.sh
@@ -5,6 +5,7 @@ dvc run -d lendingclub/modeling/09_score.py \
     -d data/eval_loan_info.fth \
     -d lendingclub/modeling/models.py \
     -d lendingclub/modeling/08_train.py \
+    -d modeling \
     -o data/eval_loan_info_scored.fth \
     -f run/09_score.dvc \
     python lendingclub/modeling/09_score.py -m catboost_both
