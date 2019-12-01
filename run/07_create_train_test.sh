8a10b70a4a8da5401214e18db017a9e155d84bad
Justin Hsi <justinm.hsi@gmail.com>
2019-12-01 15:49:01 -0800

    altered some of the run/__.sh files. reran full pipeline with 100k dataset

--- run/07_create_train_test.sh
+++ run/07_create_train_test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 dvc run -d lendingclub/data_and_eval_preparation/07_create_train_test.py \
+    -d data/strange_pmt_hist_ids.pkl \
     -d data/base_loan_info.fth \
     -d data/eval_loan_info.fth \
     -o data/train_test_ids.pkl \
